692c0e470e2098a6b5caa6c66d1503257de213cd
Emil Pålsson <emil@emilpalsson.se>
2017-10-16 23:46:38 +0200

    Add some more tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -10,4 +10,19 @@ describe('Accordion', function() {
   it('should render without throwing an error', function() {
     expect(shallow(<Accordion><div data-header='Header'>Content</div></Accordion>).contains('Header')).toBe(true);
   });
+  
+  it('should render three items when passing three children', function() {
+    const accordion = shallow(<Accordion><div data-header='Header'>Content</div><div data-header='Header'>Content</div><div data-header='Header'>Content</div></Accordion>)
+    expect(accordion.children().length).toBe(3);
+  });
+  
+  it("should'nt render any child expanded when not passing selectedIndex", function() {
+    const accordion = shallow(<Accordion><div data-header='Header'>Content</div><div data-header='Header'>Content</div></Accordion>)
+    expect(accordion.childAt(1).html().includes('height:auto')).toBe(false);
+  });
+
+  it('should render second child expanded when passing selectedIndex 2', function() {
+    const accordion = shallow(<Accordion selectedIndex={1}><div data-header='Header'>Content</div><div data-header='Header'>Content</div></Accordion>)
+    expect(accordion.childAt(1).html().includes('height:auto')).toBe(true);
+  });
 });
