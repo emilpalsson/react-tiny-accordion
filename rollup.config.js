909d935074595e4e470b265bbfef9b456a0235f1
emilpalsson <emil@emilpalsson.se>
2018-10-27 00:46:34 +0200

    Upgrade rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,17 @@ import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
+  external: Object.keys(pkg.peerDependencies || {}),
   output: {
+    name: 'ReactTinyAccordion',
     file: 'dist/index.js',
-    format: 'umd'
+    format: 'umd',
+    globals: {
+      react: 'React'
+    }
   },
-  name: 'ReactTinyAccordion',
   plugins: [
     babel(),
     uglify()
-  ],
-  external: Object.keys(pkg.peerDependencies || {}),
-  globals: {
-    react: 'React'
-  }
+  ]
 }
