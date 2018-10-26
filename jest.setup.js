ac342a5d9fc1c69e96b34054b8d7c6903622d957
emilpalsson <emil@emilpalsson.se>
2018-10-27 00:40:39 +0200

    Upgrade jest (removes need to polyfill rAF/cAF)

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/facebook/jest/issues/4545
-global.requestAnimationFrame = global.requestAnimationFrame || function (cb) {
-  return setTimeout(cb, 0);
-};
-global.cancelAnimationFrame = global.cancelAnimationFrame || global.clearTimeout
