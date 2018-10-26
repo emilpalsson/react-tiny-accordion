482de0a4f177708ac153872c189fc4a6d04aacf4
emilpalsson <emil@emilpalsson.se>
2018-10-27 00:33:12 +0200

    Update dev dependencies

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,5 @@
+// https://github.com/facebook/jest/issues/4545
 global.requestAnimationFrame = global.requestAnimationFrame || function (cb) {
   return setTimeout(cb, 0);
 };
+global.cancelAnimationFrame = global.cancelAnimationFrame || global.clearTimeout
