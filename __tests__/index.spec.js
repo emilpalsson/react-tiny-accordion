d4967cc2d7e3d327c43c8f8d49be2d68080405a6
emilpalsson <emil@emilpalsson.se>
2018-11-01 00:28:07 +0100

    Add tests for onChange event

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -6,8 +6,8 @@ import Accordion, { AccordionItem } from '../src'
 
 configure({ adapter: new Adapter() })
 
-describe('Accordion', function() {
-  it('should render without throwing an error', function() {
+describe('General', function() {
+  it('should render without crashing', function() {
     const accordion = shallow(
       <Accordion>
         <AccordionItem header="Header">Content</AccordionItem>
@@ -27,7 +27,19 @@ describe('Accordion', function() {
     expect(accordion.children()).toHaveLength(3)
   })
 
-  it("shouldn't render any child expanded when not passing selectedIndex", function() {
+  it('should render correct markup', function() {
+    const accordion = shallow(
+      <Accordion>
+        <AccordionItem header="Header1">Content 1</AccordionItem>
+        <AccordionItem header="Header2">Content 2</AccordionItem>
+      </Accordion>
+    )
+    expect(accordion.html()).toEqual('<div><div><div>Header1</div><div style=\"overflow:hidden;transition:height 500ms ease;height:0\"><div>Content 1</div></div></div><div><div>Header2</div><div style=\"overflow:hidden;transition:height 500ms ease;height:0\"><div>Content 2</div></div></div></div>')
+  })
+})
+
+describe('Selected index', function() {
+  it("should render all children collapsed when not passing selectedIndex or defaultSelectedIndex", function() {
     const accordion = shallow(
       <Accordion>
         <AccordionItem header="Header">Content</AccordionItem>
@@ -37,7 +49,7 @@ describe('Accordion', function() {
     expect(accordion.html()).not.toContain('height:auto')
   })
 
-  it('should render second child expanded when passing selectedIndex 1', function() {
+  it('should render second child expanded when passing selectedIndex 1 (controlled behaviour)', function() {
     const accordion = shallow(
       <Accordion selectedIndex={1}>
         <AccordionItem header="Header">Content</AccordionItem>
@@ -45,5 +57,43 @@ describe('Accordion', function() {
       </Accordion>
     )
     expect(accordion.childAt(1).html()).toContain('height:auto')
+  })
+
+  it('should render second child expanded when passing defaultSelectedIndex 1 (uncontrolled behaviour)', function() {
+    const accordion = shallow(
+      <Accordion defaultSelectedIndex={1}>
+        <AccordionItem header="Header">Content</AccordionItem>
+        <AccordionItem header="Header">Content</AccordionItem>
+      </Accordion>
+    )
+    expect(accordion.childAt(1).html()).toContain('height:auto')
+  })
+})
+
+describe('Event handler', function() {
+  it('should call onChange with correct params when clicking a collapsed item header', function() {
+    const handleChange = jest.fn()
+    const accordion = mount(
+      <Accordion onChange={handleChange}>
+        <AccordionItem header="Header">Content</AccordionItem>
+        <AccordionItem header="Header">Content</AccordionItem>
+      </Accordion>
+    )
+    accordion.childAt(0).childAt(1).childAt(0).simulate('click')
+    expect(handleChange).toHaveBeenCalledTimes(1)
+    expect(handleChange).toHaveBeenCalledWith(1, true, 1)
+  })
+
+  it('should call onChange with correct params when clicking an expanded item header', async function() {
+    const handleChange = jest.fn()
+    const accordion = mount(
+      <Accordion onChange={handleChange} defaultSelectedIndex={1}>
+        <AccordionItem header="Header">Content</AccordionItem>
+        <AccordionItem header="Header">Content</AccordionItem>
+      </Accordion>
+    )
+    accordion.childAt(0).childAt(1).childAt(0).simulate('click')
+    expect(handleChange).toHaveBeenCalledTimes(1)
+    expect(handleChange).toHaveBeenCalledWith(1, false, -1)
   })
 })
